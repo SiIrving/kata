65e81044a6bcf9f0a940c9184fe98627b9ce910e
Simon Irving <si@sdirving.co.uk>
2016-02-24 09:42:45 +0000

    Sample (failing) test to prove JUnit works

---
+++ src/test/java/com/kata/KataTest.java
@@ -0,0 +1,16 @@
+package com.kata;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class KataTest {
+
+    @Test
+    public void test() {
+        assertEquals("Hi","Hi");
+        assertEquals("Hi","HI");
+    }
+
+
+}

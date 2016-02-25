a9f9e0816dab0b4cb245162fd226055a19b35583
Simon Irving <si@sdirving.co.uk>
2016-02-25 10:02:14 +0000

    Not much better, but prefer my implementation

--- src/test/java/com/kata/KataTest.java
+++ src/test/java/com/kata/KataTest.java
@@ -2,15 +2,47 @@ package com.kata;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class KataTest {
 
     @Test
-    public void test() {
-        assertEquals("Hi","Hi");
-        assertEquals("Hi","HI");
+    public void stringIsEmpty_addNumbersCalled_ZeroReturned() {
+        assertEquals(0, new StringCalculator().addNumbers(""));
     }
 
+    @Test
+    public void stringContainsOneNumber_addNumbersCalled_NumberIsReturned() {
+        assertEquals(1, new StringCalculator().addNumbers("1"));
+    }
 
+    @Test
+    public void stringContainsTwoNumbers_addNumbersCalled_NumberIsReturned() {
+        assertEquals(3, new StringCalculator().addNumbers("1,2"));
+    }
+
+    private class StringCalculator {
+
+        public int addNumbers(String input) {
+            return input.isEmpty()
+                        ? 0 : sumListOfInts(Arrays.asList(input.split(","))
+                                    .stream()
+                                    .map(NumberAsString
+                                            -> new Integer(NumberAsString))
+                                    .collect(Collectors.toList()));
+        }
+
+        private int sumListOfInts(List<Integer> collect) {
+            int sum = 0;
+            for (Integer integer : collect) {
+                sum = sum + integer;
+            }
+            return sum;
+        }
+
+    }
 }

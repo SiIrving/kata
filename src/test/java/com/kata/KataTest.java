e7ac3face0075d6f9e75d7666103a53961c446c2
Simon Irving <si@sdirving.co.uk>
2016-02-26 09:52:36 +0000

    Feels like plain sailing so far... fixing the one failing test could be tricky so saving my game...

--- src/test/java/com/kata/KataTest.java
+++ src/test/java/com/kata/KataTest.java
@@ -2,14 +2,54 @@ package com.kata;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static junit.framework.TestCase.assertEquals;
 
 public class KataTest {
 
     @Test
-    public void test() {
-        assertEquals("Hi","Hi");
-        assertEquals("Hi","HI");
+    public void inputStringIsEmpty_addCalled_ZeroReturned() {
+        assertEquals(0, new StringCalculator().add(""));
+    }
+
+    @Test
+    public void inputStringContainsOneNumber_addCalled_NumberReturned() {
+        assertEquals(1, new StringCalculator().add("1"));
+    }
+
+    @Test
+    public void inputStringContainsTwoNumbers_addCalled_SumOfNumbersReturned() {
+        assertEquals(3, new StringCalculator().add("1,2"));
+    }
+
+    @Test
+    public void inputStringContainsThreeNumbers_addCalled_SumOfNumbersReturned() {
+        assertEquals(6, new StringCalculator().add("1,2,3"));
+    }
+
+    @Test
+    public void inputStringContainsThreeNumbersWithNewLineAndCommaDelimeters_addCalled_SumOfNumbersReturned() {
+        assertEquals(6, new StringCalculator().add("1\n2,3"));
+    }
+
+    @Test
+    public void inputStringContainsThreeNumbersWithSpecifiedDelimeter_addCalled_SumOfNumbersReturned() {
+        assertEquals(2, new StringCalculator().add("//;\n1;2"));
+    }
+
+    public class StringCalculator {
+        public int add(String numbers) {
+            return numbers.isEmpty() ? 0
+                    : sumStringOfNumbers(numbers);
+        }
+    }
+
+    private int sumStringOfNumbers(String numbers) {
+        return Arrays.asList(numbers.split("[,\n]"))
+                .stream()
+                .mapToInt(String -> Integer.parseInt(String))
+                .sum();
     }
 
 

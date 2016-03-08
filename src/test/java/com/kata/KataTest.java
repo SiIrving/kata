64b3a4a98494692c42c15985a6452213a039ca91
Simon Irving <si@sdirving.co.uk>
2016-03-08 09:54:02 +0000

    Test splitting with optional delim

--- src/test/java/com/kata/KataTest.java
+++ src/test/java/com/kata/KataTest.java
@@ -4,6 +4,9 @@ import io.swagger.models.auth.In;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -39,12 +42,33 @@ public class KataTest {
         assertEquals(6,kata.add("1\n2,3"));
     }
 
+    @Test
+    public void stringContainsFourNumbersWithDifferentDelimitersIncludingSpecified_SplitIntoStringOfNumbersCalled_StringOfNumbersReturned() {
+        Kata kata = new Kata();
+        assertEquals(Arrays.asList("1","2","3","4"),kata.splitIntoStringOfNumbers("//;\n1\n2,3;4"));
+    }
+
     private class Kata {
+
+        private static final String OPTIONAL_DELIM_PATTERN = "//(.{1})\n";
+
         public int add(String s) {
             return s.isEmpty() ? 0 : Arrays.asList(s.split("[,\n]"))
                                         .stream()
                                         .mapToInt(String -> Integer.parseInt(String))
                                         .sum();
+        }
+
+        public List<String> splitIntoStringOfNumbers(String s) {
+            Matcher matcher = Pattern.compile(OPTIONAL_DELIM_PATTERN + ".*")
+                                    .matcher(s);
+            String optionalDelim = "";
+            if (matcher.find()) {
+                optionalDelim = matcher.group(1);
+                s = s.replaceAll(OPTIONAL_DELIM_PATTERN, "");
+            }
+
+            return Arrays.asList(s.split("[,\n" + optionalDelim + "]"));
         }
     }
 }

4045d815c512e98e58d334773587ca33123dd93c
Simon Irving <si@sdirving.co.uk>
2016-03-08 09:42:44 +0000

    Step 1 - zero for empty string

--- src/test/java/com/kata/KataTest.java
+++ src/test/java/com/kata/KataTest.java
@@ -7,10 +7,15 @@ import static junit.framework.TestCase.assertEquals;
 public class KataTest {
 
     @Test
-    public void test() {
-        assertEquals("Hi","Hi");
-        assertEquals("Hi","HI");
+    public void stringIsEmpty_AddCalled_ZeroReturned() {
+        Kata kata = new Kata();
+        assertEquals(0,kata.add(""));
     }
 
 
+    private class Kata {
+        public int add(String s) {
+            return 0;
+        }
+    }
 }

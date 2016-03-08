e6148e8f7666a6a62de52f13ead31f4b4d572f24
Simon Irving <si@sdirving.co.uk>
2016-03-08 09:47:16 +0000

    Three numbers - incl different delims

--- src/test/java/com/kata/KataTest.java
+++ src/test/java/com/kata/KataTest.java
@@ -27,9 +27,21 @@ public class KataTest {
         assertEquals(3,kata.add("1,2"));
     }
 
+    @Test
+    public void stringContainsThreeNumbers_AddCalled_SumReturned() {
+        Kata kata = new Kata();
+        assertEquals(6,kata.add("1,2,3"));
+    }
+
+    @Test
+    public void stringContainsThreeNumbersWithDifferentDelimiters_AddCalled_SumReturned() {
+        Kata kata = new Kata();
+        assertEquals(6,kata.add("1\n2,3"));
+    }
+
     private class Kata {
         public int add(String s) {
-            return s.isEmpty() ? 0 : Arrays.asList(s.split(","))
+            return s.isEmpty() ? 0 : Arrays.asList(s.split("[,\n]"))
                                         .stream()
                                         .mapToInt(String -> Integer.parseInt(String))
                                         .sum();
